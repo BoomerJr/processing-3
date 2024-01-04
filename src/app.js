d285c95f86048f7ee9012f752f6470efc9b60544
Rosy <65262966+BoomerJr@users.noreply.github.com>
2024-01-04 18:20:20 +0000

    got it mostly working still a wip!

--- src/app.js
+++ src/app.js
@@ -1,20 +1,28 @@
 const { human, humans, genHuman } = require("../functions");
 const arry = [
   new human("test first", "1 dontMatter st", [110, 4, 18], "visa"),
-  new human("test second","2 dontMatter st", [45,9,2],"visa")
+  new human("test second", "2 dontMatter st", [45, 9, 2], "visa"),
 ];
 const segregate = (ary) => {
   let pays = [];
-  let returner = {}
+  let returner = {};
   for (let i = 0; i < ary.length; i++) {
     if (!pays.includes(ary[i].payment_method)) {
       pays.push(ary[i].payment_method);
     }
   }
-  for(let i = 0;i<ary.length;i++){
-    if(!returner[pays[i]]){
-      returner[pays[i]] = pays[i]
+  for (let i = 0; i < ary.length; i++) {
+    if (!returner[pays[i]]) {
+      returner[pays[i]] = {
+        name: pays[i],
+        pushy: []
+      };
+    }
+    if (ary[i].payment_method === returner[pays[i]].name) {
+      returner[pays[i]].pushy.push(ary[i]);
     }
   }
-  return 
+  return returner;
 };
+
+console.log(segregate(arry))
